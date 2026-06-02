ee349e953edfdb3d206f1a01d3decf2be03efeca
Satyam <pmishra2425@gmail.com>
2026-06-02 12:28:10 +0530

    Updating Progress Daily

--- src/Revision_Daywise/Trial.java
+++ src/Revision_Daywise/Trial.java
@@ -1,13 +1,12 @@
 package Revision_Daywise;
 
-public class Trial {
-    public static void main(String[] args) {
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("0");
-        System.out.println(sb.toString());
-        sb.append("1");
-        System.out.println(sb.toString());
+class Trial {
+    public String mergeAlternately(String word1, String word2) {
+        int maxLength = Math.max(word1.length(),word2.length());
+        for(int i=0;i<maxLength;i++){
+            System.out.print(word1.charAt(i));
+            System.out.print(word2.charAt(i));
+        }
     }
-
+    return 0;
 }
